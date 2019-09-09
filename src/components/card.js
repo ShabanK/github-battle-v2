d8078679bea1dcf7d024543485c4614dc308b738
ShabanK <nooneyouwillknow@gmail.com>
2019-09-09 18:50:58 +0500

    fetched repos... useEFfect working fine

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Card = (props) => {
-    return <div>
-                <img src={props.img} />
-                <p>{props.name}</p>
-            </div>
-}
+const styles = {
+  margin: "0 2% 2% 2%",
+  width: "15%",
+  border: "2px solid purple"
+};
 
-export default Card
+const Card = props => {
+  return (
+    <div style={styles}>
+      <img src={props.img} alt={props.name} width="100%" height="80%" />
+      <p style={{ textAlign: "center" }}>{props.name}</p>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,22 +1,63 @@
-import React, { useState , useEffect } from 'react'
-import "./navbar.css"
-import Card from "./card"
+import React, { useState, useEffect } from "react";
+import "./navbar.css";
+import Card from "./card";
 
 const Navbar = () => {
+  const languages = ["All", "Java", "Javascript", "Python", "Ruby"];
+  const [language, setLanguage] = useState("All");
+  const [loading, setLoading] = useState(true);
+  const [repos, setRepos] = useState([]);
 
-    const [language, setLanguage] = useState("All")
-    const [loading, setLoading] = useState(false);
-    const [repos, setRepos] = useState({})
+  useEffect(() => {
+    async function callGithub() {
+      try {
+        let result = await fetch(
+          `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+        );
+        result = await result.json();
+        const firstTen = result.items.slice(0, 10);
+        console.log(firstTen, "rezieult");
+        setRepos([...firstTen]);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    callGithub();
+  }, [language]);
 
-    return(
-        <nav>
-            <button>All</button>
-            <button>Java</button>
-            <button>Javascript</button>
-            <button>Python</button>
-            <button>Ruby</button>
-        </nav>
-    )
-}
+  return (
+    <>
+      <nav>
+        {languages.map((lang, index) => {
+          return (
+            <button key={index} value={lang} onClick={() => setLanguage(lang)}>
+              {lang}
+            </button>
+          );
+        })}
+      </nav>
+      {loading ? (
+        <div
+          style={{
+            fontSize: 40,
+            textAlign: "center"
+          }}
+        >
+          {" "}
+          LOADING...{" "}
+        </div>
+      ) : (
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {repos.map((data, index) => {
+            return (
+              <Card key={index} img={data.owner.avatar_url} name={data.name} />
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
