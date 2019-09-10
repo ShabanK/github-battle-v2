d79cdb4066ac7c183a5defdf4037352f6a4581ae
ShabanK <nooneyouwillknow@gmail.com>
2019-09-10 15:02:12 +0500

    degenerate

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,36 +2,76 @@ import React, { useState, useEffect } from "react";
 import "./navbar.css";
 import Card from "./card";
 
+const axios = require("axios");
+
 const Navbar = () => {
   const languages = ["All", "Java", "Javascript", "Python", "Ruby"];
   const [language, setLanguage] = useState("All");
   const [loading, setLoading] = useState(true);
   const [repos, setRepos] = useState([]);
+  const [current10, setCurrent10] = useState([]);
+  //const [dummy, setdummy] = useState(0);
 
   useEffect(() => {
     async function callGithub() {
       try {
-        let result = await fetch(
-          `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+        const generateLanguagePromise = language =>
+          axios.get(
+            `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+          );
+        const arrayOfPromises = languages.map(currentLanguage =>
+          generateLanguagePromise(currentLanguage)
         );
-        result = await result.json();
-        const firstTen = result.items.slice(0, 10);
-        console.log(firstTen, "rezieult");
-        setRepos([...firstTen]);
-        setLoading(false);
+        const resolvedData = await Promise.all(arrayOfPromises);
+        console.log(resolvedData);
+        setRepos(resolvedData);
+
+        const result = resolvedData.filter(item => {
+          console.log(item, "item");
+          return (
+            item.config.url ===
+            `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+          );
+        });
+        console.log(result);
+        // .data.items.slice(0, 10)
       } catch (error) {
         console.log(error);
       }
     }
     callGithub();
-  }, [language]);
+
+    setLoading(false);
+  }, []);
+
+  // useEffect(() => {
+  //   setCurrent10(
+  //     repos
+  //       .filter(
+  //         item =>
+  //           item.config.url ===
+  //           `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+  //       )
+  //       .data.items.slice(0, 10)
+  //   );
+  //   console.log(current10);
+  // }, [language]);
 
   return (
     <>
       <nav>
         {languages.map((lang, index) => {
-          return (
-            <button key={index} value={lang} onClick={() => setLanguage(lang)}>
+          return lang === language ? (
+            <button style={{ color: "orange" }} key={index} value={lang}>
+              {lang}
+            </button>
+          ) : (
+            <button
+              style={{ color: "purple" }}
+              key={index}
+              value={lang}
+              onClick={() => setLanguage(lang)}
+            >
               {lang}
             </button>
           );
@@ -45,11 +85,11 @@ const Navbar = () => {
           }}
         >
           {" "}
-          LOADING...{" "}
+          LOADING{" "}
         </div>
       ) : (
         <div style={{ display: "flex", flexWrap: "wrap" }}>
-          {repos.map((data, index) => {
+          {current10.map((data, index) => {
             return (
               <Card key={index} img={data.owner.avatar_url} name={data.name} />
             );
